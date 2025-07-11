304477516c3be4a7d888e71d0a9bdd01647d917f
Andrew Leach <7387639+andyleach@users.noreply.github.com>
2025-07-11 08:40:17 -0400

    Initial Work

---
+++ src/Property.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Strucura\Schema;
+
+class Property
+{
+    private string $type;
+    private array $attributes = [];
+    private bool $required = false;
+
+    public function __construct(string $type, bool $required = false)
+    {
+        $this->type = $type;
+        $this->required = $required;
+    }
+
+    public function setAttribute(string $key, $value): self
+    {
+        $this->attributes[$key] = $value;
+        return $this;
+    }
+
+    public function isRequired(): bool
+    {
+        return $this->required;
+    }
+
+    public function toArray(): array
+    {
+        return array_merge(
+            [
+                'type' => $this->type,
+                'required' => $this->required,
+            ],
+            $this->attributes
+        );
+    }
+}
---
+++ src/SchemaServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Strucura\Schema;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Strucura\Schema\Commands\SchemaCommand;
+
+class SchemaServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package->name('schema');
+    }
+}
